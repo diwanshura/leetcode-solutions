6a0e8c760f835ccb8c919974a3ee7895ca63c021
diwanshu <178132920+diwanshura@users.noreply.github.com>
2026-08-12 11:24:38 +0530

    Time: 0 ms (100%), Space: 24.5 MB (50.3%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int size=hours.size();
+        int count =0;
+        for(int i=0 ; i< size ; i++){
+            if(hours[i]>=target){
+                count++;
+            }
+        }
+        return count;
+    }
+};
