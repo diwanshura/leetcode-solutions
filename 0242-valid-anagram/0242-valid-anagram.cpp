44dae95382e9a159b7e2e23569d0cbabfc0ea207
diwanshu <178132920+diwanshura@users.noreply.github.com>
2026-08-18 21:39:18 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.06%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int arr1[26]={0};
+        for(char c: s) arr1[c-'a']++;
+        int arr2[26]={0};
+        for(char c: t) arr2[c-'a']++;
+
+        for(int i=0; i<26; i++) if(arr1[i]!=arr2[i]) return false;
+        return true;
+
+               
+    }
+};
